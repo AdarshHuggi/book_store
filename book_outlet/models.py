98ee1e8957c5e9b3296c3735e97d176e5e8b4fbf
AdarshHuggi <adarshhuggi5@gmail.com>
2023-03-02 15:10:33 +0530

    added model files

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
 # Create your models here.
 class Book(models.Model):
     title =models.CharField(max_length=100)
@@ -7,6 +8,20 @@ class Book(models.Model):
     rating =models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     year =models.IntegerField()
     is_bestselling=models.BooleanField(default=False)
+    slug= models.SlugField(default="",null=False)# harry-potter-1  format
+    
+
+    def save(self,**kwargs):
+        self.slug =slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+    def get_absolute_url(self):
+        """
+        This inbuilt function loads the data object of this class
+        """
+        return reverse("book-detail", args=[self.id])
+    
 
     def __str__(self):
         return f"{self.title}, {self.rating},{self.year}"
--- book_store/settings.py
+++ book_store/settings.py
@@ -55,8 +55,12 @@ ROOT_URLCONF = 'book_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
+        
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -117,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
