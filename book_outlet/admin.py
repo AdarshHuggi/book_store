e3ec13da6274169d08da4b236a281f45b25e9018
AdarshHuggi <adarshhuggi5@gmail.com>
2023-03-07 16:42:16 +0530

    added book ,address, country, models

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book, Author,Address
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields ={"slug":("title",)}
+    #displaying the columns in admin panel
+    list_display =("title","author","rating",)
+
+#registering the book and bookadmin models to display in admin panel
+admin.site.register(Book,BookAdmin)
+
+class AuthorAdmin(admin.ModelAdmin):
+
+    #displaying the columns in admin panel
+    list_display =("first_name","last_name","address",)
+#registering the Author and AuthorAdmin to display in admin panel
+admin.site.register(Author,AuthorAdmin)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    #displaying the columns in admin panel
+    list_display =("street","city","postal_code",)
+
+admin.site.register(Address,AddressAdmin)
